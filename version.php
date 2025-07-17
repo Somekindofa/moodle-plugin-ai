c85650ecf97f924a62a90d70f3adbddc69a51e96
theosaurus <93818867+Somekindofa@users.noreply.github.com>
2025-07-17 16:02:45 +0200

    Increment version number to 2025071700

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_aiassistant';
-$plugin->version = 2024011500;
+$plugin->version = 2025071700; // Increment version number
 $plugin->requires = 2022112800; // Moodle 4.1
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '1.0.0';
