f00841f91dedaf89befbf1c1b742caf0fb29a493
theosaurus <93818867+Somekindofa@users.noreply.github.com>
2025-07-28 14:31:24 +0200

    Add settings for Fireworks account ID and API token in block_aiassistant

---
+++ settings.php
@@ -0,0 +1,23 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+if ($ADMIN->fulltree) {
+    
+    $settings->add(new admin_setting_configtext(
+        'block_aiassistant/fireworks_account_id',
+        get_string('fireworks_account_id', 'block_aiassistant'),
+        get_string('fireworks_account_id_desc', 'block_aiassistant'),
+        '',
+        PARAM_TEXT
+    ));
+    
+    $settings->add(new admin_setting_configtext(
+        'block_aiassistant/fireworks_api_token',
+        get_string('fireworks_api_token', 'block_aiassistant'),
+        get_string('fireworks_api_token_desc', 'block_aiassistant'),
+        '',
+        PARAM_TEXT
+    ));
+}
