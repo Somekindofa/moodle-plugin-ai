a53d3aa15da5056031b7f2f68b6b0b1e122a062b
theosaurus <93818867+Somekindofa@users.noreply.github.com>
2025-07-28 17:22:10 +0200

    Fix settings addition for fireworks account ID and API token in settings.php

--- settings.php
+++ settings.php
@@ -2,20 +2,22 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
-    $settings->add(new admin_setting_configtext(
-        'block_aiassistant_fireworks_account_id',
+    $setting->add(new admin_setting_configtext(
+        'block_aiassistant/fireworks_account_id',
         get_string('fireworks_account_id', 'block_aiassistant'),
         get_string('fireworks_account_id_desc', 'block_aiassistant'),
         '',
         PARAM_TEXT
     ));
+    $settings->add($setting);
 
-    $settings->add(new admin_setting_configtext(
-        'block_aiassistant_fireworks_api_token',
+    $setting->add(new admin_setting_configtext(
+        'block_aiassistant/fireworks_api_token',
         get_string('fireworks_api_token', 'block_aiassistant'),
         get_string('fireworks_api_token_desc', 'block_aiassistant'),
         '',
         PARAM_TEXT
     ));
+    $settings->add($setting);
 
 }
