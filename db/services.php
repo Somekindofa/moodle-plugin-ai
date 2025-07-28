7746f5ce4a3670bf1e0ed5073280e9e8a254223a
theosaurus <93818867+Somekindofa@users.noreply.github.com>
2025-07-28 14:31:38 +0200

    Add services.php for AI assistant user credentials and chat message handling

---
+++ db/services.php
@@ -0,0 +1,23 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+
+defined('MOODLE_INTERNAL') || die();
+
+$functions = [
+    'block_aiassistant_get_user_credentials' => [
+        'classname'   => 'block_aiassistant_external',
+        'methodname'  => 'get_user_credentials',
+        'description' => 'Get user credentials for AI API access',
+        'type'        => 'read',
+        'ajax'        => true,
+        'loginrequired' => true,
+    ],
+    'block_aiassistant_send_chat_message' => [
+        'classname'   => 'block_aiassistant_external',
+        'methodname'  => 'send_chat_message',
+        'description' => 'Send chat message to AI and get response',
+        'type'        => 'write',
+        'ajax'        => true,
+        'loginrequired' => true,
+    ],
+];
