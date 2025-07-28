2e7755257fc8d3a4af2c484b97ff349aa412e5c1
theosaurus <93818867+Somekindofa@users.noreply.github.com>
2025-07-28 17:06:30 +0200

    Refactor settings.php to encapsulate settings addition within the fulltree check

--- settings.php
+++ settings.php
@@ -1,18 +1,21 @@
 <?php
 defined('MOODLE_INTERNAL') || die();
 
-$settings->add(new admin_setting_configtext(
-    'block_aiassistant/fireworks_account_id',
-    get_string('fireworks_account_id', 'block_aiassistant'),
-    get_string('fireworks_account_id_desc', 'block_aiassistant'),
-    '',
-    PARAM_TEXT
-));
+if ($ADMIN->fulltree) {
+    $settings->add(new admin_setting_configtext(
+        'block_aiassistant_fireworks_account_id',
+        get_string('fireworks_account_id', 'block_aiassistant'),
+        get_string('fireworks_account_id_desc', 'block_aiassistant'),
+        '',
+        PARAM_TEXT
+    ));
 
-$settings->add(new admin_setting_configtext(
-    'block_aiassistant/fireworks_api_token',
-    get_string('fireworks_api_token', 'block_aiassistant'),
-    get_string('fireworks_api_token_desc', 'block_aiassistant'),
-    '',
-    PARAM_TEXT
-));
+    $settings->add(new admin_setting_configtext(
+        'block_aiassistant_fireworks_api_token',
+        get_string('fireworks_api_token', 'block_aiassistant'),
+        get_string('fireworks_api_token_desc', 'block_aiassistant'),
+        '',
+        PARAM_TEXT
+    ));
+
+}
