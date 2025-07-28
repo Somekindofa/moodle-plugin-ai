db4377190644d10c99dad53c736e5f1bfedd0746
theosaurus <93818867+Somekindofa@users.noreply.github.com>
2025-07-28 16:58:27 +0200

    Refactor settings page creation by removing unnecessary conditional checks and comments

--- settings.php
+++ settings.php
@@ -1,32 +1,18 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
-
 defined('MOODLE_INTERNAL') || die();
 
-if ($ADMIN->fulltree) {
+$settings->add(new admin_setting_configtext(
+    'block_aiassistant/fireworks_account_id',
+    get_string('fireworks_account_id', 'block_aiassistant'),
+    get_string('fireworks_account_id_desc', 'block_aiassistant'),
+    '',
+    PARAM_TEXT
+));
 
-    // Create the settings page
-    $settings = new admin_settingpage(
-        'blocksettingaiassistant',
-        get_string('pluginname', 'block_aiassistant')
-    );
-
-    $settings->add(new admin_setting_configtext(
-        'block_aiassistant/fireworks_account_id',
-        get_string('fireworks_account_id', 'block_aiassistant'),
-        get_string('fireworks_account_id_desc', 'block_aiassistant'),
-        '',
-        PARAM_TEXT
-    ));
-    
-    $settings->add(new admin_setting_configtext(
-        'block_aiassistant/fireworks_api_token',
-        get_string('fireworks_api_token', 'block_aiassistant'),
-        get_string('fireworks_api_token_desc', 'block_aiassistant'),
-        '',
-        PARAM_TEXT
-    ));
-
-    // Add the settings page to the block category
-    $ADMIN->add('blocksettings', $settings);
-}
+$settings->add(new admin_setting_configtext(
+    'block_aiassistant/fireworks_api_token',
+    get_string('fireworks_api_token', 'block_aiassistant'),
+    get_string('fireworks_api_token_desc', 'block_aiassistant'),
+    '',
+    PARAM_TEXT
+));
