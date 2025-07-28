fb939768bdacf9f00188c25942fb6335d5be6d14
theosaurus <93818867+Somekindofa@users.noreply.github.com>
2025-07-28 16:42:03 +0200

    Remove commented-out code for settings page creation in settings.php

--- settings.php
+++ settings.php
@@ -4,10 +4,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
-    // Create the settings page
-    $settings = new admin_settingpage('blocksettingaiassistant', 
-                                     get_string('pluginname', 'block_aiassistant'));
-
     $settings->add(new admin_setting_configtext(
         'block_aiassistant/fireworks_account_id',
         get_string('fireworks_account_id', 'block_aiassistant'),
@@ -23,7 +19,4 @@ if ($ADMIN->fulltree) {
         '',
         PARAM_TEXT
     ));
-
-    // Register the settings page
-    $ADMIN->add('blocksettings', $settings);
 }
