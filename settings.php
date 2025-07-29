16aaed8a69f6eda1feea4b884449fc2339828e26
Somekindofa <akbas.theo@protonmail.com>
2025-07-29 13:52:47 +0200

    Fix variable name typo in settings addition for fireworks account ID and API token

--- settings.php
+++ settings.php
@@ -2,22 +2,19 @@
 defined('MOODLE_INTERNAL') || die();
 
 if ($ADMIN->fulltree) {
-    $setting->add(new admin_setting_configtext(
+    $settings->add(new admin_setting_configtext(
         'block_aiassistant/fireworks_account_id',
         get_string('fireworks_account_id', 'block_aiassistant'),
         get_string('fireworks_account_id_desc', 'block_aiassistant'),
         '',
         PARAM_TEXT
     ));
-    $settings->add($setting);
 
-    $setting->add(new admin_setting_configtext(
+    $settings->add(new admin_setting_configtext(
         'block_aiassistant/fireworks_api_token',
         get_string('fireworks_api_token', 'block_aiassistant'),
         get_string('fireworks_api_token_desc', 'block_aiassistant'),
         '',
         PARAM_TEXT
     ));
-    $settings->add($setting);
-
 }
