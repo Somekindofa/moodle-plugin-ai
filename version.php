7841cb8b86630d06b7692c2e74488c498ef8c24e
Somekindofa <akbas.theo@protonmail.com>
2025-07-29 14:00:07 +0200

    Update plugin version to 2025072907

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_aiassistant';
-$plugin->version = 2025072906;
+$plugin->version = 2025072907;
 $plugin->requires = 2022112800; // Moodle 4.1
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '0.1.4';
