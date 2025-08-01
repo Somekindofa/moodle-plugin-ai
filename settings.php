cb286b4d7656f06f2d2339ccd4a34c1e715e2121
theosaurus <93818867+Somekindofa@users.noreply.github.com>
2025-08-01 16:40:44 +0200

    Add fireworks service account ID setting to configuration

--- settings.php
+++ settings.php
@@ -17,4 +17,12 @@ if ($ADMIN->fulltree) {
         '',
         PARAM_TEXT
     ));
+
+    $settings->add(new admin_setting_configtext(
+        'block_aiassistant/fireworks_service_account_id',
+        get_string('fireworks_service_account_id', 'block_aiassistant'),
+        get_string('fireworks_service_account_id_desc', 'block_aiassistant'),
+        '',
+        PARAM_TEXT
+    ));
 }
