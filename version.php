4794eaa4fd31e48c80cf4adf49834b294cc9043e
Somekindofa <akbas.theo@protonmail.com>
2025-09-23 11:55:52 +0200

    Update version and requirements for Moodle compatibility

--- version.php
+++ version.php
@@ -9,7 +9,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_aiassistant';
-$plugin->version = 2025092203; // Updated version to force Moodle upgrade
-$plugin->requires = 2023081400; // Moodle 4.0.10 
+$plugin->version = 2025092300; // Updated version to force Moodle upgrade
+$plugin->requires = 2022041900; // Moodle 4.0
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '0.3.0'; // Incremented for refactoring release
