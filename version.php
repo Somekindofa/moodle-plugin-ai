6bb4c4ae157c081f37044f5553bd66cf25312c69
Somekindofa <akbas.theo@protonmail.com>
2025-12-11 16:09:04 +0100

    Update version.php for mod_aiassistant plugin conversion from block, including version and maturity adjustments

--- version.php
+++ version.php
@@ -5,11 +5,27 @@
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version metadata for the mod_aiassistant plugin.
+ *
+ * @package   mod_aiassistant
+ * @copyright 2025
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->component = 'block_aiassistant';
-$plugin->version = 2025110300; // Updated version to force Moodle upgrade for messages table
+$plugin->component = 'mod_aiassistant';
+$plugin->version = 2025121100; // Converted from block to mod plugin
 $plugin->requires = 2022041900; // Moodle 4.1
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '0.4.1'; // Table name length fixes
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = '1.0.0-alpha'; // First mod release
