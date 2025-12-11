af8386fbdcc79b455785e079a17ff99e452e85a8
Somekindofa <akbas.theo@protonmail.com>
2025-12-11 16:10:24 +0100

    feat: Update services for mod_aiassistant, renaming functions and adjusting descriptions

--- db/services.php
+++ db/services.php
@@ -1,35 +1,48 @@
 <?php
 // This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * External functions and services for the mod_aiassistant plugin.
+ *
+ * @package   mod_aiassistant
+ * @copyright 2025
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
 
 defined('MOODLE_INTERNAL') || die();
 
 $functions = [
-    'block_aiassistant_get_user_credentials' => [
-        'classname'   => 'block_aiassistant\external\get_user_credentials',
+    'mod_aiassistant_get_user_credentials' => [
+        'classname'   => 'mod_aiassistant\external\get_user_credentials',
         'methodname'  => 'get_user_credentials',
-        'description' => 'Get user credentials for AI API access',
+        'description' => 'Get user Fireworks API credentials',
         'type'        => 'read',
         'ajax'        => true,
         'loginrequired' => true,
     ],
-    'block_aiassistant_get_ai_config' => [
-        'classname'   => 'block_aiassistant\external\get_ai_config',
-        'methodname'  => 'get_ai_config',
-        'description' => 'Get AI configuration for frontend',
-        'type'        => 'read',
-        'ajax'        => true,
-        'loginrequired' => true,
-    ],
-    'block_aiassistant_manage_conversations' => [
-        'classname'   => 'block_aiassistant\external\manage_conversations',
+    'mod_aiassistant_manage_conversations' => [
+        'classname'   => 'mod_aiassistant\external\manage_conversations',
         'methodname'  => 'manage_conversations',
-        'description' => 'Manage user conversations (create, list, update, delete)',
+        'description' => 'Manage conversations for module instance',
         'type'        => 'write',
         'ajax'        => true,
         'loginrequired' => true,
     ],
-    'block_aiassistant_manage_messages' => [
-        'classname'   => 'block_aiassistant\external\manage_messages',
+    'mod_aiassistant_manage_messages' => [
+        'classname'   => 'mod_aiassistant\external\manage_messages',
         'methodname'  => 'manage_messages',
         'description' => 'Manage conversation messages (save, load)',
         'type'        => 'write',
